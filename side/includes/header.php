b98c8dc07004b09c2005ba7f10c333367cffa25e
JesperLeoMoberg <jllabtec@gmail.com>
2018-11-13 15:27:44 +0100

    db and users
    trying to fix connection with db an users.

--- side/includes/header.php
+++ side/includes/header.php
@@ -4,7 +4,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>JustAside</title>
+    <title>SDE AUTOMATION OG ROBOTTEKNOLOGI</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
--- side/index.php
+++ side/index.php
@@ -1,9 +1,10 @@
 <?php
+
+
+include 'includes/connect.php';
 include "includes/header.php";
 include "includes/navbar.php";
-include "css/main.css"
-?>
-  <h1>hallo world</h1>
-<?php
+include "includes/login.php";
+include "css/main.css";
 include "footer.php";
  ?>
