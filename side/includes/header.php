60b00f6b3bec4e101bc169040d3d9168e920e1fa
JesperLeoMoberg <36117215+JesperLeoMoberg@users.noreply.github.com>
2018-11-14 12:56:24 +0100

    Header
    ændret meta data

--- side/includes/header.php
+++ side/includes/header.php
@@ -1,10 +1,26 @@
 <?php
 
 ?>
-<html lang="en" dir="ltr">
+<html lang="da" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <title>SDE AUTOMATION OG ROBOTTEKNOLOGI</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="sde udlånsystem til automation og robotteknologi ">
+      <meta name="theme-color" content="#2196F3">
+      <meta content="" property="og:image" />
+      <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+      <meta content="SDE AUTOMATION OG ROBOTTEKNOLOGI" property="og:title" />
+      <meta content="sde udlånsystem til automation og robotteknologi" property="og:description" />
+      <meta name="revisit-after" content="2 days">
+      <meta name="keywords" content="sde,robot">
+
+      <title>SDE AUTOMATION OG ROBOTTEKNOLOGI</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="stylesheet" href="../css/main.css">
+      <!--[if lte IE 9]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
+          your browser</a> to improve your experience and security.</p>
+      <![endif]-->
   </head>
   <body>
