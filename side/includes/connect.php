eff56e12616abed37794f9c29f44d54ace07a235
Long Yong Ly <long0020@edu.sde.dk>
2018-12-13 10:15:01 +0100

    update

--- side/includes/connect.php
+++ side/includes/connect.php
@@ -3,12 +3,15 @@ class DBConnection
 {
     public function getConnection()
     {
-        $con = new mysqli("localhost", "root", "", "booking", 3306);
-        if (mysqli_connect_errno()) {
-            return null;
-        } else {
-            $con->set_charset("utf8");
-            return $con;
+        try {
+            $con = new mysqli("localhost", "root", "", "booking", 3306);
+            if (mysqli_connect_errno()) {
+                return null;
+            } else {
+                $con->set_charset("utf8");
+                return $con;
+            }
+        } catch (Exception $e) {
         }
     }
 }
