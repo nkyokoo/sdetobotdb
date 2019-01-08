4da7184f05f3a59927d3c4baf0b9a98083c1fb13
Long Ly <long0020@edu.sde.dk>
2019-01-08 08:53:14 +0100

    Update idletimer.js

--- side/assets/js/idletimer.js
+++ side/assets/js/idletimer.js
@@ -1,39 +1,41 @@
+
 let timeout;
 let idleTimer = 0;
-let time;
+//Check Mouse Activity
+$(document).ready(function () {
 
 
+    timeout = setInterval(checkIfIdle, 1000) ;// 1 second = 1000ms
 
-  $(document).ready(function() {
+    //check if mouse is moving
+    document.onmousemove = function(){
+        idleTimer = 0;
 
-      timeout = setInterval(function(){checkIfIdle();}, 1000) ;// 1 second = 1000ms
+    };
 
-      //check if mouse is moving
-      document.onmousemove = function(){
-          idleTimer = 0;
-      };
+    //check if you press any key.
+    document.onkeypress = function () {
+        idleTimer = 0;
 
-      //check if you press any key.
-      document.onkeypress = function () {
-          idleTimer = 0;
-      };
+    };
 
-  });
-
-
-  //check if you're idle too long and incrementing.
-  function checkIfIdle() {
-      idleTimer++;
-
-      if (idleTimer > 9){ //10 min
-          //Stop the timer
-          clearInterval(timeout);
-          //popup message or Timeout box if any.
-
-          //Go to a Page
-      }
-  };
+});
 
 
+//check if you're idle too long and incrementing.
+function checkIfIdle() {
+    idleTimer++;
+    let idleLimit = 29;
+    if (idleTimer > idleLimit){ //Max idle LIMIT
+        //Stop the timer
 
-export {time};
+        clearInterval(timeout);
+        //popup message or Timeout box if any.
+
+        //Reload Page.
+        location.reload();
+
+
+    }
+}
+
