42ab79294c568925fc784ba64732e314c6cd496d
JesperLeoMoberg <jllabtec@gmail.com>
2019-01-23 10:26:14 +0100

    Delete file side because of conflict on file that dont exist

--- side/api/api_bookingsend.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../backend/bookingsend.php";
-
-try {
-    $class = new bookingSend();
-    $class->sendBooking();
-} catch (Exception $e) {
-    die("Error: ".$e->getMessage());
-}
--- side/api/sendEmail.php
+++
@@ -1,5 +0,0 @@
-<?php
- include "../backend/mailer.php";
-  $mailfactory = new Mailer();
-
-  $mailfactory->__constructMail($get['title'], $get['content']);
