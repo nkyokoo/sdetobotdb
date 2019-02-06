53d7516c11ad5c5ca27425338e7f62ad4e5392ac
Jesper <36117215+JesperLeoMoberg@users.noreply.github.com>
2019-02-06 14:54:47 +0100

    Add files via upload

--- booking.php
+++ booking.php
@@ -15,35 +15,35 @@ include ("includes/navbar.php");
 <button onclick="ChangeLayers()">Change Layer</button>
 <p>Layer <p id="layerText">1</p> </p>
 
-<form action="api/api_bookingsend.php" method="post">
+<!-- <form action="api/api_bookingsend.php" method="post"> -->
     <div id="layer_1" class="layer1" style="display: block" >
         <span id="increment_1" style="display: none">1</span>
 
         <div id="select_list_1">
-            <div>
-                <select id="item_1" name="item_1" onchange="ChangeEnhed(1)">
-                    <?php
-                    include_once ('api/api_dropdownlistproducts_function.php');
-                    ?>
+            <div id="rows">
+            <!--  <select id="item_1" name="item_1" onchange="ChangeEnhed(1)">
                 </select>
                 <select id="enhed_1" name="enhed_1">
                     <option>Select an Item</option>
-                </select>
+                </select>-->
+                <?php
+                include_once ('api/api_dropdownlistproducts_function.php');
+                ?>
             </div>
         </div>
-        <button type="button" onclick="AddSelect(0);">Add New Item</button>
+      <!--  <button type="button" onclick="AddSelect(0);">Add New Item</button> -->
 
     </div>
 
     <!-- ################################################################################################################ -->
 
-    <div id="layer_2" class="layer2" style="display: none" >
+   <!-- <div id="layer_2" class="layer2" style="display: none" >
         <span id="increment_1" style="display: none">1</span>
         <div id="select_list_2">
             <div>
                 <select id="item_11" name="item_11" onchange="ChangeEnhed(11)">
                     <?php
-                    $class->addProductsForSelection();
+                 //   $class->addProductsForSelection();
                     ?>
                 </select>
                 <select id="enhed_11" name="enhed_11">
@@ -53,11 +53,11 @@ include ("includes/navbar.php");
         </div>
         <button type="button" onclick="AddSelect(1)">Add New Item</button>
 
-    </div>
+    </div> -->
 
 
-    <input type="submit" value="Book">
-</form>
+  <!--  <input type="submit" value="Book">
+</form> -->
 
 <?php
 include "includes/footer.php";
