570c56d071ffca4e171eb1831eb3f7bcbdc8e046
Jesper <36117215+JesperLeoMoberg@users.noreply.github.com>
2019-02-06 14:56:03 +0100

    Add files via upload

--- backend/bookingdropdownlist.php
+++ backend/bookingdropdownlist.php
@@ -1,44 +1,46 @@
 <?php
-include "../includes/connect.php";
+require_once "C:\Users\aznzl\Desktop\Github\sdetobotdb\side\includes\connect.php";
 class bookingSelectEnheder{
 
     //Populate Units/Enhed Selection box For the choosing Product
-    public function createOptionsForSelection(){
+    public function createOptionsForSelection($item){
 
         try {
             $con = new DBConnection();
             $mysqli = $con->getConnection();
-            $selectedItem = $_POST['item_id'];
-
+          //  $selectedItem = $_POST['item_id'];
+            $item = mysqli_real_escape_string($mysqli,$item);
             //Check if the chosen Product is a Product.
-            if (!empty($_POST['item_id'])) {
+                $string = '';
                 $enhedCounter = 1;
                     //Select all Units of specific Product where Status is 1 => Available
-                $sql = "SELECT id FROM product_unit_e where products_id = " . $selectedItem . " AND current_status_id = 1";
+                $sql = "SELECT id FROM product_unit_e where products_id = " . $item . " AND current_status_id = 1";
                 $result = $mysqli->query($sql);
 
                 //Check if there's any Units/Enhed available
                 if ($result->num_rows > 0) {
-                    echo '<option value="">Select Enheder</option>';
+                   // echo '<option value="">Select Enheder</option>';
                     //Fetch Units/Enhed Data from the Database.
                     while ($row = $result->fetch_assoc()) {
 
-
-                        echo '<option value="' . $enhedCounter . '">' . $enhedCounter . '</option>';
+                        $string .= '<option value="' . $enhedCounter . '">' . $enhedCounter . '</option>';
                         $enhedCounter += 1;
                         // }
 
                     }
+                    $mysqli->close();
+
+                    return $string;
                 }
                 // if none Units/Enhed available
                 else {
-                    echo '<option value="">Ingen Enheder Ledige</option>';
+                    $mysqli->close();
+                    return $string = '<option value="">Ingen Enheder Ledige</option>';
 
                 }
 
 
-            }
-            $mysqli->close();
+
         } catch (Exception $e) {
         }
     }
--- backend/dropdownlistproducts_function.php
+++ backend/dropdownlistproducts_function.php
@@ -1,42 +1,47 @@
 <?php
-include "C:\Users\aznzl\Desktop\Github\sdetobotdb\side\includes\connect.php";
-
+require_once "C:\Users\aznzl\Desktop\Github\sdetobotdb\side\includes\connect.php";
+require_once "bookingdropdownlist.php";
 class dropDownlistProducts_Function{
 
 
     function addProductsForSelection(){
 
         try {
+            $class = new  bookingSelectEnheder();
             $connection = new DBConnection();
             $mysqli = $connection->getConnection();
 
-            //Current products in use
-            $selectedSelections = $_POST['selectedProducts'];
-
-            //To make sure the variable isn't null or NaN
-            if (!isset($selectedSelections)){
-                $selectedSelections = "0";
-            }
             //Select products to Selection box which you haven't choosing yet
             // WHERE id NOT IN () is a feature of excluding specific IDs, can query without.
-            $sql = "SELECT id,product_name FROM school_products WHERE id NOT IN (".$selectedSelections.")";
+            $sql = "SELECT id,product_name,description,movable FROM school_products";
             $result = $mysqli->query($sql);
+            echo "<div class='card'>";
+            echo "<div class='card-body'>";
+            echo "<h5 class='card-header'>Featured Products</h5>";
             if ($result->num_rows > 0) {
-
                 //Default Selection
-                echo "<option value=''>Select Item</option>";
+
 
                 //Populate Selection box with data from DB
                 while ($row = $result->fetch_assoc()) {
+                    $item = $class->createOptionsForSelection($row['id']);
 
 
-
-                    echo "<option value=" . $row['id'] . ">" . $row['product_name'] . "</option>";
+                    echo "<div class='row'> <div class='col'>
+                          <div class='card'>
+                          <div class='card-body'>
+                          <h5 class='card-title'>" .$row['product_name']."</h5>
+                          <h6 class='card-subtitle mb-2 text-muted'>Moveable: ".$row['movable']."</h6>
+                          <p class='card-text'>".$row['description'].".</p>
+                          <select id='product-unit-".$row['id']."'>".$item."</select><button id='btn-".$row['id']."')>Add to Cart</button></div>
+                          </div>
+                          </div>
+                          </div>";
 
 
                 }
-
             }
+            echo "</div></div>";
 
             $mysqli->close();
         } catch (Exception $e) {
