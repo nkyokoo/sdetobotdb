ae1c8c58e755f99150a7005ed64dc44c7fead9fa
Long Yong Ly <long0020@edu.sde.dk>
2019-03-05 14:46:03 +0100

    update

--- admin/adminprotection.php
+++ admin/adminprotection.php
@@ -2,8 +2,13 @@
 
 /*
  *  //This has to be applied to all admin pages.
+ * Brug $_SESSION['returntag'] for at tjekke med så du ikke skal lave en ny session variable, da du bruger session
+ * returntag som gruppe id, ligesom i sidebar.php
  *
+ * Pseudo Code ----------------------------------------------------------
  *
- * Check if user has admin access
- * if not then return to index.php
+ * Check if user doesn't have admin access
+ *      return to index.php
+ * if user has admin access
+ *      do nothing and let nature take its course-
  */
