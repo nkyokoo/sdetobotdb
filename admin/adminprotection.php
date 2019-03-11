172a523037eeca21d7fabaabf016f636428331a3
Long Yong Ly <long0020@edu.sde.dk>
2019-03-11 12:26:03 +0100

    update

--- admin/adminprotection.php
+++ admin/adminprotection.php
@@ -3,7 +3,7 @@ session_start();
 /*
  * This file has to be applied on all admin pages as the first line of code.
  *
- * Brug $_SESSION['user_groupe_id'] for at tjekke med så du ikke skal lave en ny session variable, da du bruger session
+ * Brug $_SESSION['user_group_id'] for at tjekke med så du ikke skal lave en ny session variable, da du bruger session
  * returntag som gruppe id, ligesom i sidebar.php
  *
  * Pseudo Code ----------------------------------------------------------
@@ -13,14 +13,11 @@ session_start();
  * if user has admin access
  *      do nothing and let nature take its course-
  */
-
 // if usergroup is not set yet go back to index
-if (!isset($_SESSION['user_groupe_id'])){
+if (!isset($_SESSION['user_group_id'])){
     header('location: ../index.php');
 }
-
 // if usergroup isn't right go back to index
-if ($_SESSION['user_groupe_id'] !== 1){
+if ($_SESSION['user_group_id'] !== 1){
     header('location: ../index.php');
-
 }
