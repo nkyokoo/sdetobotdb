7d1e5c5be598f4f7895285ab4ac6fba152a221e5
JesperLeoMoberg <jllabtec@gmail.com>
2019-03-25 14:53:50 +0100

    Create api_wishlistrequest.js
    api for wishlistrequest

---
+++ api/routes/request/api_wishlistrequest.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+    method:'GET',
+    path:'/api/request/get',
+    config: {auth: false},
+    handler: async (request, h) => {
+        const pool = request.mysql.pool
+
+        try {
+            const [rows, fields] = await pool.query('select * from wish_list')
+            return rows
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
